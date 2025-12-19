ad586eac2f5628d8ebc1fb20de6709854e547668
waseemdilsher <waseemmayaa@gmail.com>
2025-12-19 20:39:44 +0500

    force install

--- Casks/deploybar.rb
+++ Casks/deploybar.rb
@@ -4,6 +4,7 @@ cask "deploybar" do
   arch arm: "aarch64", intel: "x64"
   
   url "https://raw.githubusercontent.com/waseemmaya/deploy-bar-updates/main/DeployBar_#{arch}.dmg"
+  sha256 arm: "2f54c1070b832cea6cab652fbaec50ccced8e4908bdad0240f7d0662005fdc92"
   name "DeployBar"
   desc "Monitor Vercel deployments and GitHub Actions in your menu bar"
   homepage "https://waseemmaya.github.io/deploy-bar-updates/"
@@ -16,6 +17,8 @@ cask "deploybar" do
   end
 
   app "DeployBar.app"
+
+  uninstall quit: "com.waseem.deploybar"
 
   zap trash: [
     "~/Library/Application Support/com.waseem.deploybar",
